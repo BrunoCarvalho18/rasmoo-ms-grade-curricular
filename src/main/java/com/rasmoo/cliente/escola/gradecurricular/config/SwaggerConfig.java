26f06e84206846b3b558acc2df20e22320b97212
unknown <brunocarvalhodesa@gmail.com>
2020-09-07 12:49:47 -0300

    alteracoes na api

---
+++ src/main/java/com/rasmoo/cliente/escola/gradecurricular/config/SwaggerConfig.java
@@ -0,0 +1,61 @@
+package com.rasmoo.cliente.escola.gradecurricular.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Tag;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+	
+	public static final String CURSO = "Curso";
+	public static final String MATERIA = "Matéria";
+	
+	
+	@Bean
+	public Docket gradeCurricularApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				 .apis(RequestHandlerSelectors.basePackage("com.rasmoo.cliente.escola.gradecurricular"))
+				 .build()
+				 .apiInfo(this.metaData())
+				 .tags(new Tag(CURSO,"Operações referentes a manipulação")).tags(new Tag(MATERIA,"Operações referentes a manipulação da entidade Materia."));
+		         
+	}
+	
+	@Bean
+    public LinkDiscoverers discoverers() {
+        List<LinkDiscoverer> plugins = new ArrayList<>();
+        plugins.add(new CollectionJsonLinkDiscoverer());
+        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+
+    }
+	
+
+	private ApiInfo metaData() {
+		return new ApiInfoBuilder().title("GRADE CURRICULAR")
+				.description("Api responsável pela manutenção da grande curricular RASMOO SCHOOL")
+				.version("1.0.0").license("").build();
+	}
+	
+	@Override
+	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:META-INF/resources/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:META-INF/resources/webjars/");
+	}
+
+}
